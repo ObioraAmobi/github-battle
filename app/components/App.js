d43e8e0c3a996e8476b95e2f00202c0d08a0fb85
Obiora Amobi <obiora.amobi@gmail.com>
2017-05-22 12:40:44 +0100

    Ajax / Lifecycle Events

--- app/components/App.js
+++ app/components/App.js
@@ -2,24 +2,11 @@ var React = require('react');
 var Popular = require('./Popular')
 
 class App extends React.Component {
-	constructor() {
-		super(props);
-		this.state = {
-			selectedLanguage: 'All'
-		};
-	}
 	render() {
-		var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 		return (
-			<ul className='languages'>
-				{languages.map(function (lang) {
-					return (
-						<li key={lang}>
-							{lang}
-						</li>
-					)
-				})}
-			</ul>
+			<div className='container'>
+				<Popular />
+			</div>
 		)
 	}
 }
